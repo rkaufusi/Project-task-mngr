1de5fcd1a8ccc82fbaa8fa61b78a2aba80e3193c
rkaufusi <rhettkaufusi@gmail.com>
2021-10-20 21:54:17 -0700

    added dao, route, controller

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.use("./api/v1/tasklist", tasklist)
+app.use("/api/v1/tasklist", tasklist)
 app.use("*", (req, res) => res.status(404).json({error: "not found"}));
 
 export default app
